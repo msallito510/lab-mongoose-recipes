d1fd184d642cec48e602fc1a81592129dacb048f
msallito510 <msallito510@gmail.com>
2020-02-26 20:22:07 +0100

    iteration 2 and 3

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
+
+const myRecipe = mongoose.model("Recipe", Recipe);
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err))
+  .then(() => {
+    Recipe.insertMany(data);
+  })
+  .then(recipes => {
+    recipes.array.forEach(element => console.log(element.title));
+  });
