4f0638d929e51282b55f540be62629ef78554acb
msallito510 <msallito510@gmail.com>
2020-02-26 21:37:47 +0100

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require("./data.js"); // Import of the data from './data.js'
 
-const myRecipe = mongoose.model("Recipe", Recipe);
+// const recipeModel = mongoose.model("Recipe", Recipe);
 
 // Connection to the database "recipeApp"
 mongoose
@@ -12,12 +12,37 @@ mongoose
     useUnifiedTopology: true
   })
   .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! 🔌 Database name: "${x.connections[0].name}"`
+    )
   )
   .catch(err => console.error("Error connecting to mongo", err))
   .then(() => {
-    Recipe.insertMany(data);
+    return Recipe.insertMany(data);
   })
-  .then(recipes => {
-    recipes.array.forEach(element => console.log(element.title));
-  });
+  .then(recipe => {
+    recipe.forEach(element => console.log(`👉🏽${element.title}`));
+  })
+  .then(() => {
+    Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+  })
+  .then(() => {
+    Recipe.findOne({ title: "Rigatoni alla Genovese" }, function(
+      error,
+      newDuration
+    ) {
+      if (newDuration.duration === 100) {
+        console.log(" dutation was updated");
+      }
+    });
+  })
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Carrot Cake was removed successfully 👍🏽  !!");
+  })
+  .then(() => {
+    mongoose.connection.close();
+  })
+  .catch(err => console.error("Error closing mongo 🖖🏽 ", err));
